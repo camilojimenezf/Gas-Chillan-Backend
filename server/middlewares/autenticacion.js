035496cb252e4870b503e714a182177c65634fa7
Nicolás Luna <nlunaxx@gmail.com>
2019-10-29 18:50:40 -0300

    Rutas de Login y Middlwares de Verificación

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,106 @@
+/*=================================================================================================================================*/
+// Require
+/*=================================================================================================================================*/
+const jwt = require('jsonwebtoken');
+
+/*=================================================================================================================================*/
+// Middleware para Verificar Token
+/*=================================================================================================================================*/
+let verificaToken = (req, res, next) => {
+
+    let token = req.get('token'); //para extraer el token de los headers
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            })
+        }
+
+        // agrega la propiedad usuario a la petición
+        req.user = decoded.user;
+
+        next();
+
+    });
+
+};
+
+/*=================================================================================================================================*/
+// Middleware para Verificar Rol de ADMINISTRADOR
+/*=================================================================================================================================*/
+verificaAdmin_Role = (req, res, next) => {
+
+    let user = req.user;
+
+    if (user.role !== 'ADMIN_ROLE') {
+
+        return res.status(401).json({
+            ok: false,
+            err: {
+                message: 'El usuario no tiene los permisos necesarios'
+            }
+        });
+    }
+
+    next();
+
+};
+
+
+/*=================================================================================================================================*/
+// Middleware para Verificar Rol de ADMINISTRADOR Y CHOFER
+/*=================================================================================================================================*/
+verificaAdmin_Seller_Role = (req, res, next) => {
+
+    let user = req.user;
+
+    if (user.role !== 'ADMIN_ROLE' || user.role !== 'SELLER_ROLE') {
+
+        return res.status(401).json({
+            ok: false,
+            err: {
+                message: 'El usuario no tiene los permisos'
+            }
+        });
+    }
+
+    next();
+
+};
+
+/*=================================================================================================================================*/
+// Middleware para Verificar Roles de ADMINISTRADOR y RECEPCIONISTA
+/*=================================================================================================================================*/
+verificaAdmin_Recep_Role = (req, res, next) => {
+
+    let user = req.user;
+
+    if (user.role !== 'ADMIN_ROLE' && user.role !== 'RECEPCIONIST_ROLE') {
+
+        return res.status(401).json({
+            ok: false,
+            err: {
+                message: 'El usuario no tiene los permisos'
+            }
+        });
+    };
+
+    next();
+
+};
+
+/*=================================================================================================================================*/
+// Exportación de middlewares
+/*=================================================================================================================================*/
+
+module.exports = {
+    verificaToken,
+    verificaAdmin_Role,
+    verificaAdmin_Recep_Role,
+    verificaAdmin_Seller_Role
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,63 +4,65 @@ const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const User = require('../models/user');
 
-app.get('/users', function (req, res) {
-    
+const { verificaToken, verificaAdmin_Recep_Role } = require('../middlewares/autenticacion');
+
+app.get('/users', verificaToken, verificaAdmin_Recep_Role, function(req, res) {
+
     let desde = Number(req.query.desde) || 0;
     let limite = req.query.limite || 5;
-    limite=Number(limite);
-    
-    User.find({enabled: true}, 'name surname email role enabled img')
-            .skip(desde)   
-            .limit(limite)  
-            .exec( (err, usuarios) =>{
-                if( err ){
-                    return res.status(400).json({
-                        ok:false,
-                        err
-                    });
-                }
+    limite = Number(limite);
 
-                User.countDocuments({enabled: true}, (err,conteo)=>{
-
-                    res.json({
-                        ok:true,
-                        usuarios,
-                        cantidad: conteo
-                    });
-                    
-                })
-            });
-});
-
-app.get('/user/:id', (req,res)=>{
-
-    let id=req.params.id;
-
-    User.findById(id)
-        .exec((err, userDB)=>{
-            if(err){
-                return res.status(500).json({
-                    ok:false,
+    User.find({ enabled: true }, 'name surname email role enabled img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
                     err
                 });
             }
-            if(!userDB){
+
+            User.countDocuments({ enabled: true }, (err, conteo) => {
+
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cantidad: conteo
+                });
+
+            })
+        });
+});
+
+app.get('/user/:id', verificaToken, verificaAdmin_Recep_Role, (req, res) => {
+
+    let id = req.params.id;
+
+    User.findById(id)
+        .exec((err, userDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+            if (!userDB) {
                 return res.status(400).json({
-                    ok:false,
-                    err:{
-                        message:'ID no existe'
+                    ok: false,
+                    err: {
+                        message: 'ID no existe'
                     }
                 });
             }
             res.json({
-                ok:true,
+                ok: true,
                 user: userDB
             });
         });
 });
 
-app.post('/user', function (req, res) {
+app.post('/user', verificaToken, verificaAdmin_Recep_Role, function(req, res) {
 
     let body = req.body;
 
@@ -71,13 +73,13 @@ app.post('/user', function (req, res) {
         img: body.img,
         password: body.password, //lo sincronizamos de manera sincrona (sin usar callbacks ni promesas) y el segundo parametro
         role: body.role,
-        enabled: body.enabled    //corresponde al número de veces que se le hara hash                   
+        enabled: body.enabled //corresponde al número de veces que se le hara hash                   
     });
 
-    user.save( (err,userDB) => {
-        if( err ){
+    user.save((err, userDB) => {
+        if (err) {
             return res.status(400).json({
-                ok:false,
+                ok: false,
                 err
             });
         }
@@ -86,55 +88,55 @@ app.post('/user', function (req, res) {
             user: userDB
         })
     });
-}); 
+});
 
-app.put('/user/:id', function(req, res){
+app.put('/user/:id', verificaToken, verificaAdmin_Recep_Role, function(req, res) {
 
-    let id= req.params.id;
-    let body = _.pick(req.body, ['name','surname','img','role','enabled']) ;
+    let id = req.params.id;
+    let body = _.pick(req.body, ['name', 'surname', 'img', 'role', 'enabled']);
 
     //runValidators permite que las validaciones del Schema Usuario sean validas
-    User.findByIdAndUpdate( id, body, {new:true, runValidators:true}, (err,userDB)=>{ 
-        if( err ){
+    User.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, userDB) => {
+        if (err) {
             return res.status(400).json({
-                ok:false,
+                ok: false,
                 err
             });
         }
 
         res.json({
-            ok:true,
+            ok: true,
             user: userDB
         });
     });
 });
 
-app.delete('/user/:id', function (req, res){
+app.delete('/user/:id', verificaToken, verificaAdmin_Recep_Role, function(req, res) {
     let id = req.params.id;
     let cambiaEstado = {
         enabled: false
     };
 
-    User.findByIdAndUpdate(id, cambiaEstado ,{new:true} ,(err, usuarioBorrado)=>{
-        if( err ){
+    User.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if (err) {
             return res.status(400).json({
-                ok:false,
+                ok: false,
                 err
             });
         }
-        if( !usuarioBorrado ){
+        if (!usuarioBorrado) {
             return res.status(400).json({
-                ok:false,
-                err:{   
-                    message:'Usuario no encontrado'
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
                 }
             });
         }
         res.json({
-            ok:true,
+            ok: true,
             user: usuarioBorrado
         });
     });
 });
 
-module.exports=app;
+module.exports = app;
