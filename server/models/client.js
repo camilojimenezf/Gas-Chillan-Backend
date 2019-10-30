ff1bf1c747784431476c6a0ce880a537a570bd38
Nicolás Luna <nlunaxx@gmail.com>
2019-10-29 23:32:16 -0300

    CRUD básico de Clientes

---
+++ server/models/client.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let Schema = mongoose.Schema;
+
+let tipoClientes = {
+    values: ['COMERCIAL', 'NORMAL'],
+    message: '{VALUE} no es un tipo de cliente válido'
+}
+
+let clientSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    surname: {
+        type: String,
+        required: [true, 'El apellido es necesario']
+    },
+    rut: {
+        type: String,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: [true, 'El N° de teléfono es obligatorio'],
+        minlength: 8
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    client_type: {
+        type: String,
+        required: [true, 'El tipo de clientes es obligatorio'],
+        enum: tipoClientes
+    },
+    enabled: {
+        type: Boolean,
+        default: true
+    }
+
+
+});
+
+clientSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
+module.exports = mongoose.model('Client', clientSchema);
