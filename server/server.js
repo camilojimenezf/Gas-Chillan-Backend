789ab1822040ddc64e1c8946a14f570ea310e381
camilojimenezf <camilob270@gmail.com>
2019-10-31 21:30:26 -0300

    se agregan cors

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 //Habilitar CORS
 app.use(cors());
